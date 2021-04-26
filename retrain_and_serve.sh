d418e4e97bb5c8aabb1b2048c2b826ea66350aaa
Casper Anton Poulsen <75428146+CasperAntonPoulsen@users.noreply.github.com>
2021-04-26 16:18:12 +0200

    Update retrain_and_serve.sh

--- retrain_and_serve.sh
+++ retrain_and_serve.sh
@@ -3,6 +3,5 @@ export MLFLOW_TRACKING_URI="http://training.itu.dk:5000/"
 git pull
 rm -r model
 mlflow run .
-killall -SIGKILL mlflow
-killall -SIGKILL Worker
+killall -SIGKILL 'mlflow|gunicorn|worker|sync'
 mlflow models serve -m model
