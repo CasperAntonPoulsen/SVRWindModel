2baaf2dd69347370f0fa23e78c5d11f968bdc8f1
CasperAntonPoulsen <75428146+CasperAntonPoulsen@users.noreply.github.com>
2021-04-26 18:08:36 +0200

    Update retrain_and_serve.sh

--- retrain_and_serve.sh
+++ retrain_and_serve.sh
@@ -2,6 +2,6 @@
 export MLFLOW_TRACKING_URI="http://training.itu.dk:5000/"
 git pull
 rm -r model
-mlflow run .
+mlflow run . --experiment-name sklearnSvrWindCaap
 killall -g mlflow
 mlflow models serve -m model
